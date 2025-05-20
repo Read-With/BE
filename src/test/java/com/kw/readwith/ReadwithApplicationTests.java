3b530a281ac42898410d52b1c4dbf8258f1b95e0
JSH <king601143@gmail.com>
2025-05-21 07:46:24 +0900

    first commit

---
+++ src/test/java/com/kw/readwith/ReadwithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kw.readwith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadwithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
