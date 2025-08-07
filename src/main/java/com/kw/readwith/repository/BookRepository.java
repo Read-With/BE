44461676c651d86bdcf69e624bccaeb4465655ba
JSH <king601143@gmail.com>
2025-08-08 06:48:57 +0900

    feat: s3 업로드 구현

--- src/main/java/com/kw/readwith/repository/BookRepository.java
+++ src/main/java/com/kw/readwith/repository/BookRepository.java
@@ -2,8 +2,10 @@ package com.kw.readwith.repository;
 
 import com.kw.readwith.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByInfoUploadedTrue();
 } 
