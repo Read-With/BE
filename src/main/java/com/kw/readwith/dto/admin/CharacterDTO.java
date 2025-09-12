56ee24d21ecfaab3354c6ed5db17fb6902f1dfbf
JSH <king601143@gmail.com>
2025-09-12 20:12:30 +0900

    refactor: DTO 구조를 새로운 JSON 형식에 맞게 업데이트

--- src/main/java/com/kw/readwith/dto/admin/CharacterDTO.java
+++ src/main/java/com/kw/readwith/dto/admin/CharacterDTO.java
@@ -1,5 +1,6 @@
 package com.kw.readwith.dto.admin;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,11 +9,19 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 public class CharacterDTO {
+    @JsonProperty("common_name")
     private String common_name;
+    
+    @JsonProperty("portrait_prompt")
     private String portrait_prompt;
+    
+    @JsonProperty("main_character")
     private boolean main_character;
+    
     private String description;
+    
     private Double id;
+    
     private List<String> names;
 
     @Getter
