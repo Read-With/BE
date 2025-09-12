a9db4583d06eac32e82d2a8b39cdc94c1ec12567
JSH <king601143@gmail.com>
2025-09-12 19:58:55 +0900

    feat: AI JSON 파일내 필드값과 API 리턴값의 명명규칙 통일

--- src/main/java/com/kw/readwith/dto/graph/FineGraphEdgeDTO.java
+++ src/main/java/com/kw/readwith/dto/graph/FineGraphEdgeDTO.java
@@ -1,5 +1,6 @@
 package com.kw.readwith.dto.graph;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,10 +9,18 @@ import java.util.List;
 @Getter
 @Builder
 public class FineGraphEdgeDTO {
+    @JsonProperty("id1")
     private Long from;
+    
+    @JsonProperty("id2")
     private Long to;
-    private Double weight;
+    
+    @JsonProperty("positivity")
     private Double sentimentScore;
+    
+    @JsonProperty("count")
     private Integer interactionCount;
+    
+    @JsonProperty("relation")
     private List<String> relationTags;
 }
--- src/main/java/com/kw/readwith/dto/graph/GraphNodeDTO.java
+++ src/main/java/com/kw/readwith/dto/graph/GraphNodeDTO.java
@@ -1,16 +1,33 @@
 package com.kw.readwith.dto.graph;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 @Builder
 public class GraphNodeDTO {
     private Long id;
+    
+    @JsonProperty("common_name")
     private String label;
+    
+    @JsonProperty("main_character")
     private Boolean isMainCharacter;
+    
     private String profileImage;
     private String description;
+    
+    @JsonProperty("portrait_prompt")
     private String portraitPrompt;
-    private String names;
+    
+    private List<String> names;
+    
+    // 노드 중요도 (크기)
+    private Float weight;
+    
+    // 참고용 카운트
+    private Integer count;
 }
