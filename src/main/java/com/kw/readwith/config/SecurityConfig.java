3edaed1fbcc0cc6e0be948033ca2ca76c701f2a7
JSH <king601143@gmail.com>
2025-11-02 22:41:01 +0900

    fix: spring security 설정 변경으로 health 엔드포인트 차단해제

--- src/main/java/com/kw/readwith/config/SecurityConfig.java
+++ src/main/java/com/kw/readwith/config/SecurityConfig.java
@@ -54,7 +54,8 @@ public class SecurityConfig {
                                 "/webjars/**",
                                 "/favicon.ico",
                                 "/error",
-                                "/actuator/health"  // 헬스체크
+                                "/health",  // ELB 헬스체크 (EB 배포 필수)
+                                "/actuator/health"  // Spring Actuator 헬스체크
                         ).permitAll()
                         // 인증이 필요한 엔드포인트
                         .requestMatchers(
